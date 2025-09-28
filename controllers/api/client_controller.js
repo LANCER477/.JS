eff8059723a50b8dc2857cb17d827d46613a1935
Max <youremail@example.com>
2025-09-28 16:45:26 +0300

    com

--- controllers/api/client_controller.js
+++ controllers/api/client_controller.js
@@ -1,19 +1,36 @@
 export default class ClientController {
-  doGet(request, response, id) {
-    response.writeHead(200);
-    response.end("ClientController");
-  }
+    doGet(request, response, id) {
+        response.writeHead(200);
+        response.end("ClientController");
+    }
 
-  doPost(request, response, id) {
-    response.writeHead(200, {
-      'Content-Type': 'application/json'
-    });
-    response.end(JSON.stringify({
-      "controller": "ClientController",
-      "method": "POST"
-    }));
+    doPost(request, response, id) {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        response.end(JSON.stringify({
+            "controller": "ClientController",
+            "method": "POST"
+        }));
+    }
 
-    
-    
-  }
+    doPut(request, response, id) {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        response.end(JSON.stringify({
+            "controller": "ClientController",
+            "method": "PUT"
+        }));
+    }
+
+    doPatch(request, response, id) {
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        response.end(JSON.stringify({
+            "controller": "ClientController",
+            "method": "PATCH"
+        }));
+    }
 }
